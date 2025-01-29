f483da36173ce7a322abeaa4c48a034a82cdcf97
prateek <prateek.sharma13@ibm.com>
2025-01-29 09:38:17 +0530

    fix: updates offering icon and name for DA tile in catalog (#23)
    * refactor: minor improvements
    
    * refactor: prefix logic inclusion and renovate config update
    
    * refactor: default prefix added
    
    * test: update dependencies
    
    * SKIP UPGRADE TEST
    
    * refactor: improved prefix description

--- examples/existing-instance/outputs.tf
+++ examples/existing-instance/outputs.tf
@@ -26,13 +26,3 @@ output "guid" {
   description = "GUID of the existing watsonx Assistant instance"
   value       = module.existing_watsonx_assistant_instance.guid
 }
-
-output "plan_id" {
-  description = "Plan ID of the existing watsonx Assistant instance."
-  value       = module.existing_watsonx_assistant_instance.plan_id
-}
-
-output "dashboard_url" {
-  description = "Dashboard URL of the existing watsonx Assistant instance."
-  value       = module.existing_watsonx_assistant_instance.dashboard_url
-}
