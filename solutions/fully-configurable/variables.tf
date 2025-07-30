61356659038bc3d11d8d00f233c5c18c61a83b92
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-07-30 11:16:55 +0530

    refactor: minor changes

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -28,7 +28,7 @@ variable "existing_resource_group_name" {
 
 variable "prefix" {
   type        = string
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-wxa. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/da-implementation-guidelines.md)."
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-wxa. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 
   validation {
     # - null and empty string is allowed
@@ -46,7 +46,7 @@ variable "prefix" {
   }
   validation {
     # must not exceed 16 characters in length
-    condition     = length(var.prefix) <= 16
+    condition     = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
     error_message = "Prefix must not exceed 16 characters."
   }
 }
@@ -65,7 +65,7 @@ variable "region" {
 
 variable "plan" {
   type        = string
-  description = "The plan that is required to provision the watsonx Assistant instance. Possible values are: free, plus, enterprise. For 'free' plan, the `service_endpoints` value is ignored and the default service configuration is applied. [Learn more](https://www.ibm.com/products/watsonx-assistant/pricing)."
+  description = "The plan that is required to provision the watsonx Assistant instance. In case of Lite plan, the Service Endpoints value is ignored and the default service configuration is applied. [Learn more](https://www.ibm.com/products/watsonx-assistant/pricing)."
   default     = "enterprise"
 }
 
