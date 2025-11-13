4d3d08b0e533719a319b381377b937390340b85f
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-11-13 11:46:27 +0530

    fix: updated dashboard_url

--- main.tf
+++ main.tf
@@ -3,13 +3,15 @@
 ##############################################################################
 
 locals {
-  account_id                      = var.existing_watsonx_assistant_instance_crn != null ? module.crn_parser[0].account_id : ibm_resource_instance.watsonx_assistant_instance[0].account_id
-  watsonx_assistant_id            = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].id : ibm_resource_instance.watsonx_assistant_instance[0].id
-  watsonx_assistant_crn           = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].crn : ibm_resource_instance.watsonx_assistant_instance[0].crn
-  watsonx_assistant_guid          = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].guid : ibm_resource_instance.watsonx_assistant_instance[0].guid
-  watsonx_assistant_name          = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].resource_name : ibm_resource_instance.watsonx_assistant_instance[0].resource_name
-  watsonx_assistant_plan_id       = var.existing_watsonx_assistant_instance_crn != null ? null : ibm_resource_instance.watsonx_assistant_instance[0].resource_plan_id
-  watsonx_assistant_dashboard_url = var.existing_watsonx_assistant_instance_crn != null ? null : ibm_resource_instance.watsonx_assistant_instance[0].dashboard_url
+  account_id                = var.existing_watsonx_assistant_instance_crn != null ? module.crn_parser[0].account_id : ibm_resource_instance.watsonx_assistant_instance[0].account_id
+  watsonx_assistant_id      = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].id : ibm_resource_instance.watsonx_assistant_instance[0].id
+  watsonx_assistant_crn     = var.existing_watsonx_assistant_instance_crn != null ? var.existing_watsonx_assistant_instance_crn : ibm_resource_instance.watsonx_assistant_instance[0].crn
+  watsonx_assistant_guid    = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].guid : ibm_resource_instance.watsonx_assistant_instance[0].guid
+  watsonx_assistant_name    = var.existing_watsonx_assistant_instance_crn != null ? data.ibm_resource_instance.existing_assistant_instance[0].resource_name : ibm_resource_instance.watsonx_assistant_instance[0].resource_name
+  watsonx_assistant_plan_id = var.existing_watsonx_assistant_instance_crn != null ? null : ibm_resource_instance.watsonx_assistant_instance[0].resource_plan_id
+
+  # Temporary workaround for issue 128[https://github.com/terraform-ibm-modules/terraform-ibm-watsonx-assistant/issues/128].
+  watsonx_assistant_dashboard_url = "https://cloud.ibm.com/services/watsonx-assistant/${urlencode(local.watsonx_assistant_crn)}"
 }
 
 module "crn_parser" {
